41997e1f0909d07281e2fe7a4b29c4d8d74c71ce
Ankit Kumar <ankit.kr0048@gmail.com>
2025-02-04 23:23:10 +0530

    added fetch function data from database

--- Version0/backend/main.py
+++ Version0/backend/main.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 import mysql.connector as msql
 from mysql.connector import Error
-from langchain.chat_models import ChatOpenAI
-from langgraph.graph import Graph
+# from langchain.chat_models import ChatOpenAI
+# from langgraph.graph import Graph
 
 load_dotenv()
 
@@ -28,3 +28,23 @@ except Error as e:
     print(f"[ERROR] Connecting to database: {e}")
     db_connection = None
     cursor = None
+
+# LLM Model - ChatGPT
+# llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+
+# fetch products data
+def get_products(brand):
+    if cursor:
+        cursor.execute("SELECT * FROM products WHERE brand = %s", (brand,))
+        return cursor.fetchall()
+    return []
+
+# fetch suppliers data
+def get_suppliers(category):
+    if cursor:
+        cursor.execute("SELECT * FROM products WHERE category = %s", (category,))
+        return cursor.fetchall()
+    return []
+
+# print(get_suppliers("Textile"))
+# print(get_products("Prestige"))
